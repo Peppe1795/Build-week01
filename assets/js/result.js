58a2c1fb1c242a4fc4bcca285ced8349d800a148
Peppe1795 <petrucci238@gmail.com>
2023-04-20 00:51:03 +0200

    agg

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,42 +1,31 @@
-var successi = 9;
+var successi = 2;
 var totaleDomande = 10;
 var insuccessi = totaleDomande - successi;
-var successiPercentuale = successi/totaleDomande * 100;
-var insuccessiPercentuale = insuccessi/totaleDomande * 100;
-var testo;
+var successiPercentuale = (successi * 100) / totaleDomande;
+var insuccessiPercentuale = (insuccessi* 100) / totaleDomande;
+var testo = document.querySelector('.testoCorrect');
 var btnRateUs = document.getElementById('ultimoButton');
+var indexC = document.getElementById("successi").innerText = `${successi}/${totaleDomande} questions`;
+var indexW = document.getElementById("insuccessi").innerHTML = `${insuccessi}/${totaleDomande} questions`;
 
-document.getElementById("successiPercentuale").innerHTML= successiPercentuale + "%";
-document.getElementById("insuccessiPercentuale").innerHTML= insuccessiPercentuale + "%";
-document.getElementById("successi").innerHTML= `${successi}/${totaleDomande} questions`;
-document.getElementById("insuccessi").innerHTML= `${insuccessi}/${totaleDomande} questions`;
 
-if (successiPercentuale >= 60){
-    document.getElementById("centro1").innerHTML= "Congratulations!";
-    document.getElementById("centro2").innerHTML= "You passed your exam.";
-    document.getElementById("centro3").innerHTML= "We'll send you the certificate";
-    document.getElementById("centro4").innerHTML=  "in few minutes.";
-    document.getElementById("centro5").innerHTML= "Check your mail (including";
-    document.getElementById("centro6").innerHTML= "promotions / spam folder)";
-} else {
-    document.getElementById("centro1").innerHTML= "Sorry!";
-    document.getElementById("centro2").innerHTML= "You failed your exam.";
-    document.getElementById("centro3").innerHTML= "We can't send you the certificate ";
-    document.getElementById("centro4").innerHTML= "because you faild your test.";
-    document.getElementById("centro5").innerHTML= " ";
-    document.getElementById("centro6").innerHTML= " ";
-}
-const calcolo = document.getElementById('dio')
-let media = document.querySelector('.cerchi');
-const dashboard = function(){
-    if (successiPercentuale >= 60){
+document.getElementById("successiPercentuale").innerHTML = "Correct" + "<br>" + successiPercentuale.toFixed(1) + "%";
+document.getElementById("insuccessiPercentuale").innerHTML = "Wrong" + "<br>" + insuccessiPercentuale.toFixed(1) + "%";
 
+
+const value = () => {
+    let calcolo = document.querySelector('#dio').style.strokeDashoffset = 360 - (360 * successi)/10;
+    if(successi >= 6){
+        testo.innerHTML = `<p><span>Congratulation!</span></p><span style="color:#00ffffdf">You passed the exam.</span><p></p><p>We'll send you the certificate<br>in few minutes<br>Check your email (including <br>promotions / spam folder)</p>`;
+    }else{
+        testo.innerHTML = `<p><span>Sorry!</span></p><span style="color:red ;">You failed your exam.</span><p></p><p>We can't send you the certificate<br>because you failed your test.<br></p>`;
     }
 }
-
+value();
+value()
 const vaiArate = () => {
-   btnRateUs.addEventListener('click', function() {
-    window.open('../../feedback.html');
+    btnRateUs.addEventListener('click', function () {
+        window.open('../../feedback.html');
     });
 }
 
